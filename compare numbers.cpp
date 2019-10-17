90749b122a309637c3266929c616790705ec3496
Rohitkumar131102 <rohitrk0616626@gmail.com>
2019-10-17 22:46:57 +0530

    my first commit

---
+++ compare numbers.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+//to compare numbers
+int a,b,c;
+printf("Enter numbers to compare\n");
+scanf("%d %d %d" , &a,&b,&c);
+if (a>b,a>c)
+   printf("The number %d is greater \n" ,a);
+else if(b>c)
+   printf("The number %d is greater \n" ,b);
+else 
+   printf("The number %d is greater \n" ,c);
+ return 0;
+ 
+
+
+
+}
